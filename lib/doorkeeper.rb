5a39cec57f6575d244d433379dab2e3e47705e9a
Patrick Roberts <patrick@silentbluesystems.com>
2013-01-07 00:14:35 -0800

    Update lib/doorkeeper.rb

--- lib/doorkeeper.rb
+++ lib/doorkeeper.rb
@@ -73,7 +73,7 @@ module Doorkeeper
 
   def self.client=(model)
     raise "You already have the model #{model} configured as doorkeeper client" if @client.present?
-    model.first if Rails.env.development?
+    model.first
     @client = model
   end
 end
